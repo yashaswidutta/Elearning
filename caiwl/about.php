3d687c60821cb43b0a71ddad8c70239c6685d9f8
yasaswi <yasaswidutta666@gmail.com>
2021-04-30 18:48:46 +0530

    first commit

---
+++ caiwl/about.php
@@ -0,0 +1,3 @@
+<h1><?php echo $title;?></h1>
+
+<p>It allows you to learn from any place and any time with the help of a mobile device and internet connection. According to research more and more people in the current scenario are accessing the internet through their smartphones.</p>
